fb4e1e6905a6de40ff189f734b92eaa909c2f8e4
sharmaut <utsavsh091@gmail.com>
2025-08-11 11:44:00 +1000

    feat: Add .sort() method to NoSQL QueryBuilder

--- coffy/nosql/doclist.py
+++ coffy/nosql/doclist.py
@@ -10,12 +10,33 @@ class DocList:
     Provides methods to iterate, access by index, get length, and convert to JSON.
     """
 
-    def __init__(self, docs: list[dict]):
+    def __init__(
+        self, docs: list[dict], sort_key: str = None, sort_reverse: bool = False
+    ):
         """
         Initialize the DocList with a list of documents.
         docs -- A list of documents (dictionaries) to store in the DocList.
+        sort_key -- The key to sort the documents by.
+        sort_reverse -- Whether to sort in descending order.
         """
         self._docs = docs
+        if sort_key:
+            def sort_key_func(doc):
+                val = doc.get(sort_key)
+                if val is None:
+                    return (3, None)
+                elif isinstance(val, (int, float)):
+                    return (0, val)
+                elif isinstance(val, str):
+                    return (1, val)
+                else:
+                    return (2, str(val))
+            
+            self._docs.sort(key=sort_key_func, reverse=sort_reverse)
+            # self._docs.sort(
+            #     key=lambda doc: (doc.get(sort_key) is None, doc.get(sort_key)),
+            #     reverse=sort_reverse,
+            # )
 
     def __iter__(self):
         """
@@ -46,7 +67,14 @@ class DocList:
         """
         if not self._docs:
             return "<empty result>"
-        keys = list(self._docs[0].keys())
+
+        all_keys = []
+        for doc in self._docs:
+            for key in doc.keys():
+                if key not in all_keys:
+                    all_keys.append(key)
+
+        keys = all_keys
         header = " | ".join(keys)
         line = "-+-".join("-" * len(k) for k in keys)
         rows = []
